e92a6ce8eba7b098a9249bd267074af5f5fb4620
SummerLion <1348805983@qq.com>
2018-04-18 21:46:46 +0800

    #tomcat能正常启动，但是拦截不到请求

--- Spring/src/main/java/com/baidu/web/LoginController.java
+++ Spring/src/main/java/com/baidu/web/LoginController.java
@@ -1,14 +1,10 @@
 package com.baidu.web;
 
 import java.util.Date;
-
 import javax.servlet.http.HttpServletRequest;
-
-
 import com.baidu.domain.User;
 import com.baidu.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
